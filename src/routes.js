8ebc1c1289705bb3e5c947107f73afe564f902b2
Dirvann <vanbeverendirk@hotmail.com>
2020-05-03 18:04:21 +0200

    temp redirect stop for heroku

--- src/routes.js
+++ src/routes.js
@@ -4,14 +4,14 @@ const express = require('express')
 module.exports = (app) => {
 
     // redirect http traffic to https traffic
-    app.use('*', (req, res, next) => {
+    /*app.use('*', (req, res, next) => {
         if(!req.socket.encrypted){
             console.log('unsecure connection: redirecting..')
             res.redirect('https://' + req.headers.host + req.path)
         } else {
             next()
         }
-    })
+    })*/
 
     app.use(express.static(path.join(__dirname, '..','public')))
     app.use(express.static(path.join(__dirname, '..','node_modules')))
