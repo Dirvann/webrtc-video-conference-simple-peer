45a5710b55f96f0461bb90708cefacfcd08e3212
Dirk Vanbeveren <vanbeverendirk@hotmail.com>
2020-06-08 16:59:06 +0200

    wrong port

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const options = {
     cert: fs.readFileSync(path.join(__dirname,'..','ssl','cert.pem'), 'utf-8')
 }
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3012
 
 ////////////////////////////
 
