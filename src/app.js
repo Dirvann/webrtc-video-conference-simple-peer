09d30ccfb667e0eb38c913ffaa3ce4eaf11f1f73
evgenii <skayl8827@gmail.com>
2021-11-18 05:09:26 +0300

    fix error

--- src/app.js
+++ src/app.js
@@ -27,50 +27,5 @@ require('./socketController')(io)
 httpsServer.listen(port, () => {
     console.log(`listening on port ${port}`)
 })
-const webcamButton = document.getElementById('webcamButton');
-const webcamVideo = document.getElementById('webcamVideo');
-
-const video = document.getElementById('video');
-const mode_btn = document.getElementById('btn_mod');
-
-const local_default = document.querySelector(".local");
-const local_360 = document.querySelector(".local_360");
-const remote_default = document.querySelector(".remoute_video");
-
-
-mode_btn.onclick = () => {
-    mod = !mod;
-    console.log(mod);
-    if (mod) {
-        local_default.style.display = "none";
-        remote_default.style.display = "none";
-        local_360.style.display = "block";
-        local_360.querySelector('video').style.display = "block";
-    }
-    else {
-        local_default.style.display = "block";
-        remote_default.style.display = "block";
-        local_360.style.display = "none";
-    }
-}
-AFRAME.registerComponent('play-video', {
-    schema: {
-        target: { type: 'selector' },
-        src: { type: 'string' },
-        on: { default: 'click' },
-    },
-
-    multiple: true,
-
-    init: function () {
-        var data = this.data;
-
-        this.el.addEventListener(data.on, function () {
-            data.target.setAttribute('src', data.src);
-            data.target.components.material.material.map.image.play();
-        });
-    }
-});
-
 
 
