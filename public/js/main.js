215460d70e92561ac7d82f2f1adab8f047d572f7
evgenii <skayl8827@gmail.com>
2021-11-18 03:07:20 +0300

    http

--- public/js/main.js
+++ public/js/main.js
@@ -12,8 +12,6 @@ let localStream = null;
 let peers = {}
 
 // redirect if not https
-if(location.href.substr(0,5) !== 'https') 
-    location.href = 'https' + location.href.substr(4, location.href.length - 4)
 
 
 //////////// CONFIGURATION //////////////////
@@ -23,15 +21,15 @@ if(location.href.substr(0,5) !== 'https')
  */
 const configuration = {
     "iceServers": [{
-            "urls": "stun:stun.l.google.com:19302"
-        },
-        // public turn server from https://gist.github.com/sagivo/3a4b2f2c7ac6e1b5267c2f1f59ac6c6b
-        // set your own servers here
-        {
-            url: 'turn:192.158.29.39:3478?transport=udp',
-            credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-            username: '28224511:1379330808'
-        }
+        "urls": "stun:stun.l.google.com:19302"
+    },
+    // public turn server from https://gist.github.com/sagivo/3a4b2f2c7ac6e1b5267c2f1f59ac6c6b
+    // set your own servers here
+    {
+        url: 'turn:192.158.29.39:3478?transport=udp',
+        credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+        username: '28224511:1379330808'
+    }
     ]
 }
 
